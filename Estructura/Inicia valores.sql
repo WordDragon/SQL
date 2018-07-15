16a2b4ea738fb1eeba8bf4fc7b97cf0b82ae1d1f
WordDragon <39063264+WordDragon@users.noreply.github.com>
2018-07-14 20:24:52 -0400

    Agregando los scripts SQL

---
+++ Estructura/Inicia valores.sql
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CARGA DE VALORES INICIALES
+-- VER. AUTOR   :     Darwin Cantuta Zegobia
+-- VER. FECHA   :     2018 - Julio - 12
+-- VER. DESCR   :     Procedimiento almacenado "spBuscaUsuario" el cual busca el usuario para autenticar el acceso al sistema
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO usuario VALUES('darwin','12345')
+INSERT INTO usuario VALUES('sa','sa')
---
+++ Procedimientos/spBuscarUsuario.sql
@@ -0,0 +1,42 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CREA SP spBuscarUsuario
+-- VER. AUTOR   :     Darwin Cantuta Zegobia
+-- VER. FECHA   :     2018 - Julio - 12
+-- VER. DESCR   :     Procedimiento almacenado "spBuscarUsuario" el cual busca el usuario para autenticar el acceso al sistema
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+--USE LABIC
+
+--EXEC spBuscarUsuario 'darwin','12345'
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spBuscarUsuario')
+	DROP PROCEDURE spBuscarUsuario
+GO
+
+CREATE PROCEDURE spBuscarUsuario
+	@usrId VARCHAR(50),
+	@usrPassword VARCHAR(50)
+WITH ENCRYPTION
+AS BEGIN
+	-- DEFINE OPCIONES DE EJECUCION
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	-- DECLARA VARIABLES
+	DECLARE @strNombreSP VARCHAR(200) = 'spBuscarUsuario - ' + ISNULL(@usrId,'');
+	DECLARE @strMensajeError VARCHAR(500);
+	DECLARE @strMensajeSalida VARCHAR(5000);
+
+	BEGIN TRY
+		SELECT usrId FROM usuario WHERE usrId = @usrId AND usrPassword = @usrPassword
+	END TRY
+	BEGIN CATCH
+		DECLARE @ErrorNumber    INT				= ERROR_NUMBER(); 
+		DECLARE @ErrorMessage   NVARCHAR(4000)	= ERROR_MESSAGE();   
+		DECLARE @ErrorSeverity	INT				= ERROR_SEVERITY();
+        DECLARE @ErrorState		INT				= ERROR_STATE(); 
+		DECLARE @ErrorProcedure NVARCHAR(4000)	= ERROR_PROCEDURE();     
+		DECLARE @ErrorLine      INT				= ERROR_LINE();
+		SET @strMensajeSalida = ISNULL(@strMensajeError, '') + ' - ' + @ErrorMessage;
+	END CATCH;
+END;
